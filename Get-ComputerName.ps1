536cad61e24bdeba20e107c39fef48c1ab586fcb
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2017-12-20 00:00:30 +0100

    Update Get-ComputerName.ps1

--- Get-ComputerName.ps1
+++ Get-ComputerName.ps1
@@ -1,10 +1,33 @@
+function Main {
+  # Module can be found in 'C:\windows\system32\WindowsPowershell\v1.0\Modules'
+  # Simply copy the whole DnsClient-folder to a package and then run this script to set OSDComputerName according to the function Get-ComputerName
+  Import-Module ".\DnsClient\DnsClient.psd1"
+    try {
+        $tsenv = New-Object -ComObject "Microsoft.SMS.TSEnvironment"
+    } catch [System.Runtime.InteropServices.COMException] {
+        Write-Host "Failed to init TSEnvironment"
+    }
+
+    $tsenv.Value("OSDComputerName") = Get-ComputerName
+    Write-Host "Current value of OSDComputerName: '$($tsenv.Value("OSDComputerName"))'"
+}
+
+function Get-LUNetIPAddress {
+  Start-Process -FilePath "X:\windows\system32\ipconfig.exe" -RedirectStandardOutput 'X:\windows\temp\ip.out' -NoNewWindow -Wait
+  Get-Content -Path 'X:\windows\temp\ip.out' | ForEach-Object {
+    if ($_ -match 'IPv4' -or $_ -match 'IPv6') {
+      ($_ -split ': ')[1].Trim()
+    }
+  }
+}
+
 function Get-ComputerName {
     # Set computername to autogenerate
     $ComputerName = '*'
 
     if ($ComputerName -eq '*') {
         # Get name from dns for every IP that is available on the computer and cut off the domain, avoid the 'hosts'-file
-        $ComputerName = Get-NetIPAddress | Select-Object -ExpandProperty IPAddress | % { Resolve-DnsName -ErrorAction SilentlyContinue -DnsOnly -NoHostsFile -Type PTR -Name $_ | Where-Object { $_.Section -eq 'Answer' } | Select-Object -ExpandProperty NameHost | % { $_ -split '\.' | Select-Object -First 1 } } | Where-Object { $_ -notmatch 'localhost' }
+        $ComputerName = Get-LUNetIPAddress | % { Resolve-DnsName -ErrorAction SilentlyContinue -DnsOnly -NoHostsFile -Type PTR -Name $_ | Where-Object { $_.Section -eq 'Answer' } | Select-Object -ExpandProperty NameHost | % { $_ -split '\.' | Select-Object -First 1 } } | Where-Object { $_ -notmatch 'localhost' }
     }
     if ($ComputerName -eq '*') {
         # Get the SerialNumber and Manufacturer available in BIOS
@@ -28,3 +51,5 @@ function Get-ComputerName {
     # At this point we return w/e name we have
     return $ComputerName
 }
+
+Main
