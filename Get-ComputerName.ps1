2888488523e0f16d33e89ae29c4e3a2b79d44440
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2018-03-14 08:57:58 +0100

    Update Get-ComputerName.ps1

--- Get-ComputerName.ps1
+++ Get-ComputerName.ps1
@@ -1,35 +1,38 @@
  function Main {
-  # Module 'DnsClient' can be found in 'C:\windows\system32\WindowsPowershell\v1.0\Modules'
-  # Simply copy the whole DnsClient-folder to a package and then run this script to set OSDComputerName according to the function Get-ComputerName
-  Import-Module ".\DnsClient\DnsClient.psd1"
+    # Module 'DnsClient' can be found in 'C:\windows\system32\WindowsPowershell\v1.0\Modules'
+    # Simply copy the whole DnsClient-folder to a package and then run this script to set OSDComputerName according to the function Get-ComputerName
+    Import-Module ".\DnsClient\DnsClient.psd1"
     try {
         $tsenv = New-Object -ComObject "Microsoft.SMS.TSEnvironment"
+        $tsenv.Value("OSDComputerName") = Get-ComputerName
+        Write-Host "Current value of OSDComputerName: '$($tsenv.Value("OSDComputerName"))'"
     } catch [System.Runtime.InteropServices.COMException] {
         Write-Host "Failed to init TSEnvironment"
     }
+    if (-not $tsenv) {
+        Get-ComputerName
+    }
 
-    $tsenv.Value("OSDComputerName") = Get-ComputerName
-    Write-Host "Current value of OSDComputerName: '$($tsenv.Value("OSDComputerName"))'"
 }
 
 function Get-LUNetIPAddress {
-  Start-Process -FilePath "X:\windows\system32\ipconfig.exe" -RedirectStandardOutput 'X:\windows\temp\ip.out' -NoNewWindow -Wait
-  Get-Content -Path 'X:\windows\temp\ip.out' | ForEach-Object {
-    if ($_ -match 'IPv4' -or $_ -match 'IPv6') {
-      ($_ -split ': ')[1].Trim()
-    }
-  }
+    # This will run in WinPE, if adapter is DHCPEnabled and it has an IP we return the IP(s) of the adapter
+    Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Property IPAddress -Filter "DHCPEnabled = TRUE" | ? { ($_.IPAddress).Count -ge 1 } | Select-Object -ExpandProperty IPAddress
 }
 
 function Get-ComputerName {
-    # Set computername to autogenerate
-    $ComputerName = '*'
-
-    if ($ComputerName -eq '*') {
-        # Get name from dns for every IP that is available on the computer and cut off the domain, avoid the 'hosts'-file
-        $ComputerName = Get-LUNetIPAddress | % { Resolve-DnsName -ErrorAction SilentlyContinue -DnsOnly -NoHostsFile -Type PTR -Name $_ | Where-Object { $_.Section -eq 'Answer' } | Select-Object -ExpandProperty NameHost | % { $_ -split '\.' | Select-Object -First 1 } } | Where-Object { $_ -notmatch 'localhost' }
+    # Get name from dns for every IP that is available on the computer and cut off the domain, avoid the 'hosts'-file
+    $ComputerName = Get-LUNetIPAddress | ForEach-Object {
+        Resolve-DnsName -ErrorAction SilentlyContinue -DnsOnly -NoHostsFile -Type PTR -Name $_ | Where-Object { $_.Section -eq 'Answer' } | Select-Object -ExpandProperty NameHost | ForEach-Object {
+            $_ -split '\.' | Select-Object -First 1 
+        }
+    } | Where-Object {
+        if ($_ -notmatch 'localhost') {
+            $_
+        }
     }
-    if ($ComputerName -eq '*') {
+
+    if (-not $ComputerName) {
         # Get the SerialNumber and Manufacturer available in BIOS
         $BIOSInfo = Get-WmiObject -ErrorAction SilentlyContinue -Namespace root\cimv2 -Class Win32_BIOS -Property SerialNumber,Manufacturer
         $SerialNumber = $BIOSInfo | Select-Object -ExpandProperty SerialNumber
@@ -42,7 +45,7 @@ function Get-ComputerName {
             -replace 'LENOVO','LNVO'
         } | Where-Object { $_ -notmatch ' ' } # Add all characters that can not be part of computername
         # We need to check the length, build final name here
-        $SerialNumberAndManufacturer = "$($Manufacturer)-$($SerialNumber)"
+        $SerialNumberAndManufacturer = "$($Manufacturer)$($SerialNumber)"
         # If we have all data and its less than the maximum length, use it
         if ($Manufacturer -and $SerialNumber -and $SerialNumberAndManufacturer.Length -le 15) {
             $ComputerName = $SerialNumberAndManufacturer
