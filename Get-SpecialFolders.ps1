c7411727f5bb7f70432f093fcfc18aaa880343ce
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2018-06-14 21:33:59 +0200

    Update Get-SpecialFolders.ps1

--- Get-SpecialFolders.ps1
+++ Get-SpecialFolders.ps1
@@ -1,11 +1,6 @@
-begin {
-    $FolderNames = [Environment+SpecialFolder] | Get-Member -MemberType Property -Static | Select-Object -ExpandProperty Name
-}
-process{
-    $FolderNames | ForEach-Object {
-        New-Object PSObject -Property @{
-            Name = $_
-            Path = ([Environment]::GetFolderPath($_))
-        }
+[Environment+SpecialFolder] | Get-Member -MemberType Property -Static | Select-Object -ExpandProperty Name | ForEach-Object {
+    New-Object PSObject -Property @{
+        Name = $_
+        Path = ([Environment]::GetFolderPath($_))
     }
 }
