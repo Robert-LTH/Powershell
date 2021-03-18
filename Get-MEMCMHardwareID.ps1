0af974e0f10eeee6c2de5c5e52c76caf0e2f8604
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2021-03-18 14:54:08 +0100

    Update Get-MEMCMHardwareID.ps1

--- Get-MEMCMHardwareID.ps1
+++ Get-MEMCMHardwareID.ps1
@@ -1,8 +1,18 @@
 function Get-MEMCMHardwareID {
     [OutputType([String])]
     $SystemEnclosureInformation = Get-CimInstance -Namespace "root/cimv2" -ClassName "Win32_SystemEnclosure"
+    
+    $SystemEnclosureChassisType = $SystemEnclosureInformation | Select-Object -ExpandProperty "ChassisTypes"
+    if (Get-IsChassisTypeLaptop -ChassisType $SystemEnclosureChassisType) {
+        $MacAddress = "<Not used on laptop>"
+    }
+    else {
+        $MacAddress = Get-CimInstance -Namespace "root/cimv2" -Query "SELECT Index, MACAddress FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled=True" | Select-Object -First 1 -ExpandProperty MacAddress
+    }
+
     $SystemEnclosureSerialNumber = $SystemEnclosureInformation | Select-Object -ExpandProperty "SerialNumber"
-    if ($SystemEnclosureSerialNumber -eq 'None') {
+    
+    if ($SystemEnclosureSerialNumber -eq 'None' -or 31 -in $SystemEnclosureChassisType) {
         $SystemEnclosureSerialNumber = $null
     }
     
@@ -13,14 +23,7 @@ function Get-MEMCMHardwareID {
     
     $BaseBoardSerialNumber = Get-CimInstance -Namespace "root/cimv2" -ClassName "Win32_BaseBoard" | Select-Object -ExpandProperty "SerialNumber"
     $BIOSSerialNumber = Get-CimInstance -Namespace "root/cimv2" -ClassName "Win32_BIOS" | Select-Object -ExpandProperty "SerialNumber"
-    $SystemEnclosureChassisType = $SystemEnclosureInformation | Select-Object -ExpandProperty "ChassisTypes"
-
-    if (Get-IsChassisTypeLaptop -ChassisType $SystemEnclosureChassisType) {
-        $MacAddress = "<Not used on laptop>"
-    }
-    else {
-        $MacAddress = Get-CimInstance -Namespace "root/cimv2" -Query "SELECT Index, MACAddress FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled=True" | Select-Object -First 1 -ExpandProperty MacAddress
-    }
+    
 
     $HashBytes = Get-HashFromString -String ("{0}!{1}!{2}!{3}!{4}" -f $SystemEnclosureSerialNumber,$SystemEnclosureSMBIOSAssetTag,$BaseBoardSerialNumber,$BIOSSerialNumber,$MacAddress)
     $HashString = Get-HexAsString -Bytes $HashBytes
