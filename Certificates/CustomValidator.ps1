8a7ab7c6649f62328e17dfb80808059da541a799
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2021-11-15 10:09:34 +0100

    Create CustomValidator.ps1

---
+++ Certificates/CustomValidator.ps1
@@ -0,0 +1,14 @@
+#requires -Version 5
+#requires -PSEdition Desktop
+
+# https://stackoverflow.com/a/55246366
+
+class TrustAllCertsPolicy : System.Net.ICertificatePolicy {
+    [bool] CheckValidationResult([System.Net.ServicePoint] $a,
+                                 [System.Security.Cryptography.X509Certificates.X509Certificate] $b,
+                                 [System.Net.WebRequest] $c,
+                                 [int] $d) {
+        return $true
+    }
+}
+[System.Net.ServicePointManager]::CertificatePolicy = [TrustAllCertsPolicy]::new()
