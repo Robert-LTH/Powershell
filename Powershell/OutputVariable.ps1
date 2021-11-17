c2244c7f951c49975c0b0ea3dce11f1d93e277ef
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2021-11-17 08:36:26 +0100

    Create OutputVariable.ps1

---
+++ Powershell/OutputVariable.ps1
@@ -0,0 +1,3 @@
+# https://twitter.com/thetommymaynard/status/1456665707475701767
+# Output from all commands executed after the following will end up in the variable $__
+$PSDefaultParameterValues["Out-Default:OutVariable"] = "__"
