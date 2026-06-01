de61a9096f44ebf5c638977965b9439a7e8a639e
Robert Johnsson <32772780+Robert-LTH@users.noreply.github.com>
2026-06-01 08:09:47 +0200

    Update Write-DebouncedLog.ps1

--- Write-DebouncedLog.ps1
+++ Write-DebouncedLog.ps1
@@ -1,3 +1,99 @@
+function Start-ScriptTimer {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [scriptblock]$Action,
+
+        [int]$IntervalSeconds = 5,
+
+        [switch]$Once,
+
+        [hashtable]$MessageData = @{}
+    )
+
+    $timer = [System.Timers.Timer]::new()
+    $timer.Interval = $IntervalSeconds * 1000
+    $timer.AutoReset = -not $Once.IsPresent
+
+    $sourceIdentifier = "ScriptTimer.$([guid]::NewGuid())"
+
+    $eventJob = Register-ObjectEvent `
+        -InputObject $timer `
+        -EventName Elapsed `
+        -SourceIdentifier $sourceIdentifier `
+        -MessageData @{
+            Action = $Action
+            Data   = $MessageData
+        } `
+        -Action {
+            & $Event.MessageData.Action $Event.MessageData.Data
+        }
+
+    $timer.Start()
+
+    [pscustomobject]@{
+        Timer            = $timer
+        EventJob         = $eventJob
+        SourceIdentifier = $sourceIdentifier
+    }
+}
+
+function Stop-ScriptTimer {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        $TimerHandle
+    )
+
+    if ($TimerHandle.Timer) {
+        $TimerHandle.Timer.Stop()
+        $TimerHandle.Timer.Dispose()
+    }
+
+    if ($TimerHandle.SourceIdentifier) {
+        Unregister-Event -SourceIdentifier $TimerHandle.SourceIdentifier -ErrorAction SilentlyContinue
+    }
+
+    if ($TimerHandle.EventJob) {
+        $TimerHandle.EventJob | Remove-Job -Force -ErrorAction SilentlyContinue
+    }
+}
+
+function Restart-ScriptTimer {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [scriptblock]$Action,
+
+        [int]$IntervalSeconds = 5,
+
+        [string]$Name = 'Default',
+
+        [switch]$Once,
+
+        [hashtable]$MessageData = @{}
+    )
+
+    if (-not $script:ScriptTimers) {
+        $script:ScriptTimers = @{}
+    }
+
+    if ($script:ScriptTimers.ContainsKey($Name)) {
+        Stop-ScriptTimer -TimerHandle $script:ScriptTimers[$Name]
+        $script:ScriptTimers.Remove($Name)
+    }
+
+    $timerHandle = Start-ScriptTimer `
+        -IntervalSeconds $IntervalSeconds `
+        -Action $Action `
+        -Once:$Once `
+        -MessageData $MessageData
+
+    $script:ScriptTimers[$Name] = $timerHandle
+
+    return $timerHandle
+}
+
 function Write-DebouncedLog {
     [CmdletBinding()]
     param(
